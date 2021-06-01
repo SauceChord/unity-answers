f7a8c17004276360a5e4e46699b8d9c1f8c96687
SauceChord <58859889+SauceChord@users.noreply.github.com>
2021-06-01 18:28:49 +0200

    Add haccr original code

---
+++ Assets/Users/haccr/HookSpawner.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace haccr
+{
+    public class HookSpawner : MonoBehaviour
+    {
+        public Transform player;
+        public GameObject hook;
+        public LayerMask mask;
+        public float range = 100f;
+
+        void Start()
+        {
+            spawnHooks();
+        }
+
+        void Update()
+        {
+
+        }
+
+        void spawnHooks()
+        {
+            int nearby = 0;
+            Collider2D[] collisions = Physics2D.OverlapCircleAll(player.position, range, mask);
+            nearby = collisions.Length;
+            int totalHooks = 3;
+            for (int i = 0; i < totalHooks - nearby; i++)
+            {
+                bool hasSpawned = false;
+                bool safeArea = true;
+                while (!hasSpawned)
+                {
+                    Vector3 hookPos = new Vector3(Random.Range(-range, range), Random.Range(-range, range), 0);
+                    Collider2D[] nearbyHooks = Physics2D.OverlapCircleAll(hookPos, range, mask);
+                    foreach (Collider2D collision in nearbyHooks)
+                    {
+                        if ((hookPos - collision.gameObject.transform.position).magnitude < 3) safeArea = false;
+                    }
+                    if ((hookPos - player.position).magnitude < 3) safeArea = false;
+                    if (safeArea)
+                    {
+                        Instantiate(getHookType(), hookPos, Quaternion.identity);
+                        hasSpawned = true;
+                    }
+                }
+            }
+        }
+
+        Object getHookType()
+        {
+            return hook;
+        }
+    }
+}
