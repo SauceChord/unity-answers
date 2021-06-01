8afb70d8368dec346c93c9be7c50a641f99878ce
SauceChord <58859889+SauceChord@users.noreply.github.com>
2021-06-01 19:31:17 +0200

    Refactor code

--- Assets/Users/haccr/Hook/HookSpawner.cs
+++ Assets/Users/haccr/Hook/HookSpawner.cs
@@ -8,7 +8,8 @@ namespace haccr
         public GameObject hook;
         public LayerMask mask;
         public float range = 100f;
-
+        public float collisionDistance = 3f;
+        public int spawnAttempts = 10;
         void Start()
         {
             SpawnHooks();
@@ -16,29 +17,60 @@ namespace haccr
 
         void SpawnHooks()
         {
+            int hooks = CountHooksToSpawn();
+            for (int i = 0; i < hooks; ++i)
+                AttemptToSpawnHook();
+        }
+
+        int CountHooksToSpawn()
+        {
+            const int totalHooks = 3;
             Collider2D[] collisions = Physics2D.OverlapCircleAll(player.position, range, mask);
             int nearby = collisions.Length;
-            int totalHooks = 3;
-            for (int i = 0; i < totalHooks - nearby; i++)
+            int numHooksToSpawn = totalHooks - nearby;
+            return numHooksToSpawn;
+        }
+
+        void AttemptToSpawnHook()
+        {
+            for (int i = 0; i < spawnAttempts; ++i)
             {
-                bool hasSpawned = false;
-                bool safeArea = true;
-                for (int tries = 0; tries < 10 && !hasSpawned; ++tries)
+                Vector3 hookPos = GetRandomPosition();
+                if (!IsColliding(hookPos))
                 {
-                    Vector3 hookPos = new Vector3(Random.Range(-range, range), Random.Range(-range, range), 0);
-                    Collider2D[] nearbyHooks = Physics2D.OverlapCircleAll(hookPos, range, mask);
-                    foreach (Collider2D collision in nearbyHooks)
-                    {
-                        if ((hookPos - collision.gameObject.transform.position).magnitude < 3) safeArea = false;
-                    }
-                    if ((hookPos - player.position).magnitude < 3) safeArea = false;
-                    if (safeArea)
-                    {
-                        Instantiate(GetHookType(), hookPos, Quaternion.identity);
-                        hasSpawned = true;
-                    }
+                    Instantiate(GetHookType(), hookPos, Quaternion.identity);
+                    break;
                 }
             }
+        }
+
+        Vector3 GetRandomPosition()
+        {
+            return new Vector3(Random.Range(-range, range), Random.Range(-range, range), 0);
+        }
+
+        bool IsColliding(Vector3 hookPos)
+        {
+            return IsCollidingWithPhysics(hookPos) 
+                || IsCollidingWithPlayer(hookPos);
+        }
+
+        bool IsCollidingWithPlayer(Vector3 hookPos)
+        {
+            Vector3 distance = hookPos - player.position;
+            return distance.magnitude < collisionDistance;
+        }
+
+        bool IsCollidingWithPhysics(Vector3 hookPos)
+        {
+            Collider2D[] nearbyHooks = Physics2D.OverlapCircleAll(hookPos, range, mask);
+            foreach (Collider2D collision in nearbyHooks)
+            {
+                Vector3 distance = hookPos - collision.transform.position;
+                if (distance.magnitude < collisionDistance) 
+                    return true;
+            }
+            return false;
         }
 
         Object GetHookType()
